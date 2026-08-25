9828ee95fa0efa03f1770f3d07ff755f900a8599
dobariyapriya14 <dobariyapriya14@gmail.com>
2026-08-25 12:39:14 +0530

    Added comment for get categories

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -5,6 +5,7 @@ export interface AuthRequest extends Request {
   user?: any;
 }
 
+//Get categories
 export const getCategories = async (req: AuthRequest, res: Response): Promise<void> => {
   try {
     const filter: any = {};
